1312be238a611e708e215547283dbdccd0eb00dc
Morad Teaha <mrad1999@gmail.com>
2023-06-10 15:34:32 +0300

    clearing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React  from "react";
 import HomePage from "./pages/HomePage";
 import SignIn from "./pages/SignIn";
-import CheckExams from "./pages/CheckExams";
-import StudentExam from "./pages/StudentExam";
 import { Routes,Route } from "react-router-dom";
 import ShowData from "./pages/ShowData";
 
@@ -14,7 +12,6 @@ function App() {
       <Route path="/" element={<HomePage/>}/>
       <Route path="/SignIn" element={<SignIn/>}/>
       <Route path="/ShowData" element={<ShowData/>}/>
-      <Route path="/StudentExam" element={<StudentExam/>}/>
 
 
     </Routes>
