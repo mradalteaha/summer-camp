7fb1513ccec026502193e13085c72ccd55bad5c2
Morad Teaha <mrad1999@gmail.com>
2023-06-29 21:13:55 +0300

    شسي

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,7 @@ import React ,{useState} from "react";
 import Heading from "../components/common/Heading";
 import Footer from "../components/common/Footer";
 import WelcomeComponent from "../components/WelcomeComponent";
-import Container from "../components/common/Container";
-import OpenQuestionCard from "../components/OpenQuestionCard";
+
 
 
 function HomePage() {
